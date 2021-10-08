1b581b50904f81ceee9d5933b1fa7e194e717b9d
jnntnym <jnntnym@gmail.com>
2021-10-08 03:45:46 -0400

    7th commit

---
+++ src/test/java/stepDef/loginStep.java
@@ -0,0 +1,27 @@
+package stepDef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class loginStep {
+    @Given("I am at TalentTek Sign in page")
+    public void iAmAtTalentTekSignInPage() {
+
+    }
+
+    @And("I enter valid email address")
+    public void iEnterValidEmailAddress() {
+        
+    }
+
+    @And("I enter valid password")
+    public void iEnterValidPassword() {
+        
+    }
+
+    @When("I click on login button")
+    public void iClickOnLoginButton() {
+    }
+}
+ // When create choose Java option instead of java 8 ( java 8 has error)
---
+++ src/test/java/stepDef/profilePageStep.java
@@ -0,0 +1,11 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class profilePageStep {
+    @Then("I should be able to successfully login")
+    public void iShouldBeAbleToSuccessfullyLogin() {
+    }
+}
+
+// When create choose Java option instead of java 8 ( java 8 has error)
